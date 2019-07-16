80dfc4517ce802e26e0b13ffffdb7b17c10fa4d4
Vivek Vijayan <vivek.kply@gmail.com>
2019-07-16 19:32:49 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -30,7 +30,7 @@ function App(props) {
   const {UI:{loading}}=props;
   return (
       <div className="App">
-//       {loading && <Loader/>}
+
         <Navbar/>
         <div className="container">
         <Switch>
