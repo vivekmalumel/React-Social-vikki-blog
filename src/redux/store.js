f1bfad00cfc92eee1397d8027bbfe9437d93d62b
Vivek Vijayan <vivek.kply@gmail.com>
2019-07-16 19:39:24 +0530

    Update store.js

--- src/redux/store.js
+++ src/redux/store.js
@@ -13,6 +13,6 @@ const reducer=combineReducers({
 })
 
 const store=createStore(reducer,initialState,
-    compose(applyMiddleware(...middleware),window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()));
+    compose(applyMiddleware(...middleware)));
 
 export default store;
